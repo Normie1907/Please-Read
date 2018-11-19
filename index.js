9b19a0847d88f195498165cffdd302d43aca10fe
Normie1907 <45154936+Normie1907@users.noreply.github.com>
2018-11-19 17:54:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-your dumbass is here because you're the retard who likes stalking my profile and hacking my bots
-fuck you
-I hate you
-do something with your life
-stop being retarded and stop destroying things other people make
-go back to 4chan where your fatass belongs
+no bot token here, haha
